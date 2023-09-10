fb2b9e91f6433b27351757167e388cb080ff70ec
far-id <fcrizkywijaya@gmail.com>
2023-09-10 16:21:30 +0700

    migration

---
+++ database/migrations/2023_09_10_085937_create_started_files_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\File;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('started_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(File::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('started_files');
+    }
+};
