f72decd02c19515e580f08f0ab278cbeb24192af
far-id <fcrizkywijaya@gmail.com>
2023-10-30 12:03:39 +0700

    remove useless dashboard

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::controller(FileController::class)
     ->middleware(['auth', 'verified'])
     ->group(function () {
